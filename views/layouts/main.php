ad309a7d179ec848c0d19a8d87e4b2a66ee876fb
Konstantin <wusnoop@gmail.com>
2022-04-27 18:56:18 +0300

    first step

---
+++ views/layouts/main.php
@@ -0,0 +1,54 @@
+<?php
+
+/** @var yii\web\View $this */
+/** @var string $content */
+
+use app\assets\AppAsset;
+use app\widgets\Alert;
+use yii\bootstrap4\Breadcrumbs;
+use yii\bootstrap4\Html;
+use yii\bootstrap4\Nav;
+use yii\bootstrap4\NavBar;
+
+AppAsset::register($this);
+?>
+<?php $this->beginPage() ?>
+<!DOCTYPE html>
+<html lang="<?= Yii::$app->language ?>" class="h-100">
+<head>
+    <meta charset="<?= Yii::$app->charset ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <?php $this->registerCsrfMetaTags() ?>
+    <title><?= Html::encode($this->title) ?></title>
+    <?php $this->head() ?>
+</head>
+<body class="d-flex flex-column h-100">
+<?php $this->beginBody() ?>
+<section class="body">
+    <header>
+        <div class="container">
+            <div class="header">
+                <a href="index.html">На главную</a>
+                <a href="#">Вход в админку</a>
+                <a href="#">Корзина</a>
+                <input type="text" style="padding: 5px" placeholder="Поиск..." name="search">
+            </div>
+        </div>
+    </header>
+    <container>
+        <?= $content ?>
+    </container>
+
+
+    <footer>
+        <div class="container">
+            <div class="footer">
+                &copy; Все права защищены или типа того
+            </div>
+        </div>
+    </footer>
+</section>
+<?php $this->endBody() ?>
+</body>
+</html>
+<?php $this->endPage() ?>
